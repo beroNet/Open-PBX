5ba05ca628a05271e0a67337ab06383924fc5fed
sebastianertz <sebastian.ertz@tk-ertz.de>
2013-02-18 11:38:32 +0100

    adaptation to new Snom XML-Browser class #8

--- template/www/phones/snom/menu/index.php
+++ template/www/phones/snom/menu/index.php
@@ -2,194 +2,143 @@
 
 include('/apps/OpenPBX/www/includes/variables.php');
 include(BAF_APP_WWW . '/phones/snom/menu/functions.php');
+include(BAF_APP_WWW . '/phones/snom/menu/xml_browser_func.php');
 
 $url_base = 'http://' . $_SERVER['SERVER_NAME'] . BAF_URL_BASE . '/phones/snom/menu/index.php';
 
 // add softkeys for main-menu and exit on all pages
-$softkey_items['F1']['label'] =		'Main';
-$softkey_items['F1']['url'] =		$url_base;
-$softkey_items['F4']['label'] =		'Exit';
-$softkey_items['F4']['action'] =	'F_ABORT';
+$softkeys = new SnomIPPhoneSoftkeys();
+$softkeys->addURL('F1', $url_base, 'Main');
+$softkeys->addAction('F4', 'F_ABORT', 'Exit');
 
-switch ($_GET['page']) {
-case 'diversion':
-	$title = 'Call Diversion';
-
-	$menu_items['All Calls'] =		$url_base . '?page=diversion_gen';
-	$menu_items['Line Busy'] =		$url_base . '?page=line_busy';
-	$menu_items['Not Available'] =		$url_base . '?page=not_avail';
-
-	$softkey_items['F2']['label'] =		'Back';
-	$softkey_items['F2']['url'] =		$url_base;
-	break;
-case 'diversion_gen':
-	$title = 'Diversion of All Calls';
-
-	$extension = get_extension_by_ip($_SERVER['REMOTE_ADDR']);
-	$number = get_number_by_extension($extension, '/CFWD/');
-
-	switch($number) {
-	case 0:
-	case '':
-		$menu_items['Off'] =		$url_base . '?page=server_gen&busy=0';
-		$menu_items['Phone Number'] =	$url_base . '?page=phone_gen';
-		$menu_items['Mailbox'] =	$url_base . '?page=server_gen&busy=vb';
-		break;
-	case 'vb':
-		$menu_items['Off'] =		$url_base . '?page=server_gen&busy=0';
-		break;
-	default:
-		$menu_items['Off'] =		$url_base . '?page=server_gen&busy=0';
-		$menu_items['Phone Number'] =	$url_base . '?page=phone_gen&number=' . $number;
-		$menu_items['Mailbox'] =	$url_base . '?page=server_gen&busy=vb';
-		break;
-	}
-
-	$softkey_items['F2']['label'] =		'Back';
-	$softkey_items['F2']['url'] =		$url_base . '?page=diversion';
-	break;
-case 'line_busy':
-	$title = 'Diversion if Line Busy';
-
-	$extension = get_extension_by_ip($_SERVER['REMOTE_ADDR']);
-	$number = get_number_by_extension($extension, '/CFB/');
-
-	switch ($number) {
-	case 0:
-	case '':
-	case 'vb':
-		$menu_items['Off'] =		$url_base . '?page=server_busy&busy=0';
-		$menu_items['Phone Number'] =	$url_base . '?page=phone_busy';
-		$menu_items['Mailbox'] =	$url_base . '?page=server_busy&busy=vb';
-		break;
-	default:
-		$menu_items['Off'] =		$url_base . '?page=server_busy&busy=0';
-		$menu_items['Phone Number'] =	$url_base . '?page=phone_busy&number=' . $number;
-		$menu_items['Mailbox'] =	$url_base . '?page=server_busy&busy=vb';
-		break;
-	}
-
-	$softkey_items['F2']['label'] =		'Back';
-	$softkey_items['F2']['url'] =		$url_base . '?page=diversion';
-	break;
-case 'not_avail':
-	$title = 'Diversion if Not Available';
-
-	$extension = get_extension_by_ip($_SERVER['REMOTE_ADDR']);
-	$number = get_number_by_extension($extension, '/CFU/');
-
-	switch($number) {
-	case 0:
-	case '':
-	case 'vb':
-		$menu_items['Off'] =		$url_base . '?page=server_notav&busy=0';
-		$menu_items['Phone Number'] =	$url_base . '?page=phone_notav';
-		$menu_items['Mailbox'] =	$url_base . '?page=server_notav&busy=vb';
-		break;
-	default:
-		$menu_items['Off'] =		$url_base . '?page=server_notav&busy=0';
-		$menu_items['Phone Number'] =	$url_base . '?page=phone_notav&number=' . $number;
-		$menu_items['Mailbox'] =	$url_base . '?page=server_notav&busy=vb';
-		break;
-	}
-
-	$softkey_items['F2']['label'] =		'Back';
-	$softkey_items['F2']['url'] =		$url_base . '?page=diversion';
-	break;
-case 'phone_gen':
-	$title = 'Divert All Calls to:';
-
-	$prompt_items['Prompt'] = $url_base . '?page=server_gen';
-
-	$input_items['Phone Number']['query'] = 'opt=busy&number';
-	$input_items['Phone Number']['default'] = $_GET['number'];
-	$input_items['Phone Number']['input_flags'] = 'n';
-	break;
-case 'phone_busy':
-	$title = 'If Busy divert Calls to:';
-
-	$prompt_items['Prompt'] = $url_base . '?page=server_busy';
-
-	$input_items['Phone Number']['query'] = 'opt=busy&number';
-	$input_items['Phone Number']['default'] = $_GET['number'];
-	$input_items['Phone Number']['input_flags'] = 'n';
-	break;
-case 'phone_notav':
-	$title = 'If Not Available divert Calls to:';
-
-	$prompt_items['Prompt'] = $url_base . '?page=server_notav';
-
-	$input_items['Phone Number']['query'] = 'opt=busy&number';
-	$input_items['Phone Number']['default'] = $_GET['number'];
-	$input_items['Phone Number']['input_flags'] = 'n';
-	break;
-case 'server_gen':
-	$title = 'Success';
-
-	if (!empty($_GET['busy'])) {
-		$fwd_tgt = $_GET['busy'];
-		$text =	'Call Diversion set to ' . (($fwd_tgt == 'vb') ? 'Mailbox' : $fwd_tgt) . ' for All Calls.';
-	} elseif (!empty($_GET['number'])) {
-		$fwd_tgt = $_GET['number'];
-		$text =	'Call Diversion set to ' . (($fwd_tgt == 'vb') ? 'Mailbox' : $fwd_tgt) . ' for All Calls.';
-	} else {
-		$fwd_tgt = '0';
-		$text = 'Call Diversion disabled for All Calls.';
-	}
-
-	$extension = get_extension_by_ip($_SERVER['REMOTE_ADDR']);
-	set_forwarding_by_extension($extension, 'CFWD', $fwd_tgt);
-
-	$softkey_items['F2']['label'] =		'Back';
-	$softkey_items['F2']['url'] =		$url_base . '?page=diversion_gen';
-	break;
-case 'server_busy':
-	$title = 'Success';
-
-	if (!empty($_GET['busy'])) {
-		$fwd_tgt = $_GET['busy'];
-		$text =	'Call Diversion set to ' . (($fwd_tgt == 'vb') ? 'Mailbox' : $fwd_tgt) . ' for Line Busy.';
-	} else if (!empty($_GET['number'])) {
-		$fwd_tgt = $_GET['number'];
-		$text =	'Call Diversion set to ' . (($fwd_tgt == 'vb') ? 'Mailbox' : $fwd_tgt) . ' for Line Busy.';
-	} else {
-		$fwd_tgt = '0';
-		$text = 'Call Diversion disabled for Line Busy.';
-	}
-
-	$extension = get_extension_by_ip($_SERVER['REMOTE_ADDR']);
-	set_forwarding_by_extension($extension, 'CFB', $fwd_tgt);
-
-	$softkey_items['F2']['label'] =		'Back';
-	$softkey_items['F2']['url'] =		$url_base . '?page=line_busy';
-	break;
-case 'server_notav':
-	$title = 'Success';
-
-	if (!empty($_GET['busy'])) {
-		$fwd_tgt = $_GET['busy'];
-		$text =	'Call Diversion set to ' . (($fwd_tgt == 'vb') ? 'Mailbox' : $fwd_tgt) . ' for Not Available.';
-	} else if (!empty($_GET['number'])) {
-		$fwd_tgt = $_GET['number'];
-		$text =	'Call Diversion set to ' . (($fwd_tgt == 'vb') ? 'Mailbox' : $fwd_tgt) . ' for Not Available.';
-	} else {
-		$fwd_tgt = '0';
-		$text = 'Call Diversion disabled for Not Available.';
-	}
-
-	$extension = get_extension_by_ip($_SERVER['REMOTE_ADDR']);
-	set_forwarding_by_extension($extension, 'CFU', $fwd_tgt);
-
-	$softkey_items['F2']['label'] =		'Back';
-	$softkey_items['F2']['url'] =		$url_base . '?page=not_avail';
-	break;
-default:
-	$title = 'OpenPBX Main Menu';
-
-	$menu_items['Call Diversion'] = $url_base . '?page=diversion';
-	break;
+$page = trim($_REQUEST['page']);
+if (! in_array($page, array('call_diversion'))) {
+	$page = false;
 }
 
-echo build_page($text, $title, $menu_items, $softkey_items, $input_items, $prompt_items);
+if (!$page) {
+
+	$snom_xml = new SnomIPPhoneMenu();
+	$snom_xml->setTitle('OpenPBX Main Menu');
+	$snom_xml->add('Call Diversion', $url_base . '?page=call_diversion');
+
+	$softkeys->addAction('Cancel', 'F_ABORT');
+
+} elseif ($page === 'call_diversion') {
+
+	// CFWD = All Calls
+	// CFB  = Line Busy
+	// CFU  = Not Available
+	$type = trim($_REQUEST['type']);
+	if (! in_array($type, array('CFWD','CFB','CFU'))) {
+		$type = false;
+	}
+
+	if (!$type) {
+
+		$snom_xml = new SnomIPPhoneMenu();
+		$snom_xml->setTitle('Call Diversion');
+		$snom_xml->add('All Calls', $url_base . '?page=call_diversion&type=CFWD');
+		$snom_xml->add('Line Busy', $url_base . '?page=call_diversion&type=CFB');
+		$snom_xml->add('Not Available', $url_base . '?page=call_diversion&type=CFU');
+
+		$softkeys->addURL('F2', $url_base, 'Back');
+		$softkeys->addURL('Cancel', $url_base);
+
+	} else {
+
+		$busy = trim($_REQUEST['busy']);
+		if (! in_array($busy, array('off','number','vb'))) {
+			$busy = false;
+		}
+
+		$number = trim($_REQUEST['number']);
+
+		$extension = get_extension_by_ip($_SERVER['REMOTE_ADDR']);
+		$fwd_tgt = get_number_by_extension($extension, $type);
+
+		if (!$busy) {
+
+			$snom_xml = new SnomIPPhoneMenu();
+
+			if ($type == 'CFWD') {
+				$snom_xml->setTitle('Diversion of All Calls');
+			} elseif ($type == 'CFB') {
+				$snom_xml->setTitle('Diversion if Line Busy');
+			} elseif ($type == 'CFU') {
+				$snom_xml->setTitle('Diversion if Not Available');
+			}
+
+			$snom_xml->add('Off', $url_base . '?page=call_diversion&type=' . $type . '&busy=off');
+			$snom_xml->add('Phone Number', $url_base . '?page=call_diversion&type=' . $type . '&busy=number');
+			$snom_xml->add('Mailbox', $url_base . '?page=call_diversion&type=' . $type . '&busy=vb');
+
+			if ($fwd_tgt == 'vb') {
+				$snom_xml->select('Mailbox');
+			} elseif ($fwd_tgt != '0' && strlen($fwd_tgt) > 1) {
+				$snom_xml->select('Phone Number');
+			} else {
+				$snom_xml->select('Off');
+			}
+
+			$url_back = $url_base . '?page=call_diversion';
+			$softkeys->addURL('F2', $url_back, 'Back');
+			$softkeys->addURL('Cancel', $url_back);
+
+		} elseif ($busy == 'number' && strlen($number) == 0) {
+
+			if ($fwd_tgt != '0' && $fwd_tgt != 'vb') {
+				$number = $fwd_tgt;
+			} else {
+				$number = false;
+			}
+
+			$snom_xml = new SnomIPPhoneInput();
+			$snom_xml->setURL($url_base . '?page=call_diversion&type=' . $type . '&busy=number');
+			$snom_xml->add('number','Phone Number','n',$number);
+
+			$url_back = $url_base . '?page=call_diversion&type=' . $type;
+			$softkeys->addURL('F2', $url_back, 'Back');
+			$softkeys->addURL('Cancel', $url_back);
+
+		} else {
+
+			$text = 'Call Diversion';
+
+			if ($busy == 'off') {
+				set_forwarding_by_extension($extension, $type, '0');
+				$text .= ' disabled';
+			} elseif ($busy == 'vb') {
+				set_forwarding_by_extension($extension, $type, 'vb');
+				$text .= ' set to Mailbox';
+			} elseif ($busy == 'number') {
+				set_forwarding_by_extension($extension, $type, $number);
+				$text .= ' set to ' . $number;
+			}
+
+			if ($type == 'CFWD') {
+				$text .= ' for All Calls.';
+			} elseif ($type == 'CFB') {
+				$text .= ' for Line Busy.';
+			} elseif ($type == 'CFU') {
+				$text .= ' for Not Available.';
+			}
+
+			$snom_xml = new SnomIPPhoneText();
+			$snom_xml->setTitle('Success');
+			$snom_xml->setText($text);
+
+			$url_back = $url_base . '?page=call_diversion&type=' . $type;
+			$softkeys->addURL('F2', $url_back, 'Back');
+			$softkeys->addURL('Cancel', $url_back);
+
+		}
+	}
+}
+
+
+$snom_xml->setSoftkeys($softkeys);
+
+$snom_xml->show();
 
 ?>
