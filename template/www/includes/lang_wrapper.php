a20472bb78c6ecbb02c0067e2ce899b225d20a93
Florian Kraatz <fk@beronet.com>
2013-02-18 11:16:01 +0100

    fixed language is always en on Firefox issue

--- template/www/includes/lang_wrapper.php
+++ template/www/includes/lang_wrapper.php
@@ -13,10 +13,19 @@ if ($handle = opendir(BAF_APP_WWW . '/includes/lang/')) {
 
 # get the language the user has configured
 $lang_file = 'en.php';
+$lang_set = 0;
 if (!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
 	foreach (explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']) as $user_lang) {
-		if (in_array($user_lang, $lang_codes)) {
-			$lang_file = $user_lang . '.php';
+		foreach ($lang_codes as $lang_code) {
+			if (strtolower($lang_code) == strtolower($user_lang)) {
+				$lang_file = $lang_code . '.php';
+				$lang_set = 1;
+				break;
+			}
+		}
+
+		if ($lang_set == 1) {
+			break;
 		}
 	}
 }
