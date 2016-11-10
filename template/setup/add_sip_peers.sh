8869e18507a50618cf55241e35b5862c0a71a6ff
Florian Kraatz <fk@beronet.com>
2016-11-10 17:28:50 +0100

    testing and using new location of ini_to_db.php

--- template/setup/add_sip_peers.sh
+++ template/setup/add_sip_peers.sh
@@ -10,9 +10,9 @@ cat <<EOF
 [$name]
 address = $localip:25060
 register = 0
-user = $name 
-secret = $name 
-externip = 
+user = $name
+secret = $name
+externip =
 config = t38=1,ie_on_sip=0,wait_for_cancel=0,dtmfmode=rfc2833,s2i_cpt=,i2s_cpt=,failover_proxy=,failover_timeout=0,from_sip_src_setting=from_user,sip_from_user_setting=\${account_username},sip_from_display_setting=\${new_source}
 
 EOF
@@ -20,11 +20,15 @@ EOF
 
 function print_if_not_existing {
 	name=$1
-	if ! grep "\[$name\]" /usr/conf/isgw.sip > /dev/null  ; then 
+	if ! grep "\[$name\]" /usr/conf/isgw.sip > /dev/null  ; then
 		print_sip_peer $name
 	fi
 }
 
 print_if_not_existing openpbx-gateway >> /usr/conf/isgw.sip
 
-/usr/bin/env -i bash -c "/usr/local/www/berogui/misc/ini_to_db.php"  2>/dev/null
+if [ -f /usr/local/php/ini_to_db.php ]; then
+	/usr/bin/env -i bash -c "/usr/local/php/ini_to_db.php" 2>/dev/null
+else
+	/usr/bin/env -i bash -c "/usr/local/www/berogui/misc/ini_to_db.php"  2>/dev/null
+fi
